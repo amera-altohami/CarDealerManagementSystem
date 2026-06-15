559b59b35bdfdccc94a2a03b230f697218e2939e
amera-altohami <ameraaltohami@gmail.com>
2026-06-15 23:33:28 +0200

    setup firebase

---
+++ src/lib/firebase.ts
@@ -0,0 +1,19 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDJdrt80on3blndGnu7oOj4UqRZtytOupM",
+  authDomain: "car-dealer-management-system.firebaseapp.com",
+  projectId: "car-dealer-management-system",
+  storageBucket: "car-dealer-management-system.firebasestorage.app",
+  messagingSenderId: "964803983764",
+  appId: "1:964803983764:web:8406440a15cd407b33b781",
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const db = getFirestore(app);
+export const auth = getAuth(app);
+
+export default app;
